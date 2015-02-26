15031810f31fed38128c41ad3f1d898372546b70
gsrujana <srujana.gattupalli@mavs.uta.edu>
2015-02-26 16:37:16 -0600

    Change print statements to C#
    Changed all System.out.println to Console.WriteLine

--- ConsoleApplication4/AssignmentExpression.cs
+++ ConsoleApplication4/AssignmentExpression.cs
@@ -150,7 +150,7 @@ namespace edu.uta.cse.proggen.expressions
 						}
 						builder.Append(lhs + " = (" + lhs.Type + ")" + result + "\n\n");
 						builder.Append("} catch(Exception e) { e.printStackTrace(); }\n\n");
-                        builder.Append("System.out.println(" + lhs + ");\n");
+                        builder.Append("Console.WriteLine(" + lhs + ");\n");
 						output += builder.ToString();
                         
 					}
